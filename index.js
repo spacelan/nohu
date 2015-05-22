d02cd9664df5b013518ab21608b0992cea44beca
SPACELAN <spacelan1993@gmail.com>
2015-05-22 23:42:31 +0800

    http to https

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var restify = require('restify');
 var routes = require('./routes');
 var logger = require('morgan');
+var fs = require('fs');
 
-var server = restify.createServer();
+var certificate = fs.readFileSync('cert/certificate.pem', 'utf8');
+var key = fs.readFileSync('cert/privatekey.pem', 'utf8');
+
+var server = restify.createServer({
+  certificate: certificate,
+  key: key
+});
 server.use(logger('dev'));
 server.use(restify.dateParser());
 server.use(restify.queryParser());
@@ -10,6 +17,11 @@ server.use(restify.gzipResponse());
 server.use(restify.bodyParser());
 
 routes(server);
+
+server.get('/', function(req, res, next) {
+  res.send('nohunohu~');
+  return next();
+});
 
 server.listen(3000, function() {
   console.log('%s listening at %s', server.name, server.url);
